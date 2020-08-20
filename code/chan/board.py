253980007251c124f5108644573ef173e96247ec
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2020-08-20 00:11:51 -0400

    Introduce 4chan posts

---
+++ code/chan/board.py
@@ -0,0 +1,44 @@
+import json
+
+from tqdm import tqdm
+
+import sys
+sys.path.append('code/')
+
+from base.stream import Stream
+from chan.post import ChanPost
+
+
+class Board(Stream):
+
+    def __init__(self, name, uid=None):
+        super().__init__(name, domain='4chan', uid=uid)
+
+    def load_from_json(self, path):
+        data = json.load(open(path))
+
+        posts = {}
+        for k, datum in tqdm(data.items()):
+            uid = int(k)
+
+            post = ChanPost(uid)
+            post.load_from_file(datum)
+
+            posts[uid] = post
+
+        for uid, post in tqdm(posts.items()):
+            pid = post.pid
+            if pid:
+                posts[pid].comments[uid] = post
+            else:
+                self.add_post(posts[uid])
+
+        # for now keeping this as is...
+        # TODO: Recovering the broader quote structure through ">>{UID}"
+
+
+if __name__ == '__main__':
+    board_path = 'data/news.json'
+    news = Board(name='Current News', uid='/news/')
+    news.load_from_json(board_path)
+    news.stat()
---
+++ code/chan/post.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+
+import sys
+sys.path.append('code/')
+
+from base.post import Post
+
+
+class ChanPost(Post):
+
+    def __init__(self, uid):
+        super().__init__(uid)
+
+    def __hash__(self):
+        return self._uid
+
+    @staticmethod
+    def format_time(timestr):
+        if type(timestr) == int:
+            return datetime.fromtimestamp(timestr)
+        else:
+            raise ValueError()
+
+    def load_from_file(self, raw_thread):
+        # booleans
+        prop = 'sticky'
+        self.meta[prop] = raw_thread.get(prop, -1) == 1
+
+        prop = 'closed'
+        self.meta[prop] = raw_thread.get(prop, -1) == 1
+
+        prop = 'bumplimit'
+        self.meta[prop] = raw_thread.get(prop, -1) == 1
+
+        prop = 'imagelimit'
+        self.meta[prop] = raw_thread.get(prop, -1) == 1
+
+        prop = 'archived'
+        self.meta[prop] = raw_thread.get(prop, -1) == 1
+
+        # time
+        self.set_time(raw_thread['time'])
+
+        # text
+        prop = 'semantic_tag'
+        self.meta[prop] = raw_thread.get(prop, None)
+
+        prop = 'sub'
+        self.meta[prop] = raw_thread.get(prop, '')
+
+        prop = 'com'
+        self.meta[prop] = raw_thread.get(prop, '')
+
+        text = (self.meta['sub'] + ' ' + self.meta['com']).strip()
+        text = text.replace('<br>', '\n')
+        text = text.replace('<br/>', '\n')
+        text = text.replace('<wbr>', '')
+        text = text.replace('<wbr/>', '')
+
+        soup = BeautifulSoup(text, 'html.parser')
+        self.text = soup.text
+
+        # int
+        prop = 'unique_ips'
+        self.meta[prop] = raw_thread.get(prop, None)
+
+        prop = 'replies'
+        self.meta[prop] = raw_thread.get(prop, None)
+
+        prop = 'resto'
+        self.meta[prop] = raw_thread.get(prop, None)
+        self.pid = raw_thread.get(prop, None)
+
